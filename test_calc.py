81ffa390ea4a419f00ee7a92e1b596b3225e1e26
rodolfohub <42880262+rodolfohub@users.noreply.github.com>
2018-09-11 20:45:40 -0300

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -2,9 +2,9 @@ import pytest
 from calc import soma
 
 def tesscalc():
-	assert soma(1, 1) == 2
-	assert soma(-1, 1) == 0
-	assert soma('1', 1) == 2
-	assert soma('-1', 1) == 0
-	assert soma(1.5, 1) == 2.5
-	assert soma('-1,5', 1) == -0.5
+ assert soma(1, 1) == 2
+ assert soma(-1, 1) == 0
+ assert soma('1', 1) == 2
+ assert soma('-1', 1) == 0
+ assert soma(1.5, 1) == 2.5
+ assert soma('-1,5', 1) == -0.5
