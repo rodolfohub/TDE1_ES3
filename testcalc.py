c25692f54d146874bb9ba945c239e3c149d7aeb3
rodolfohub <42880262+rodolfohub@users.noreply.github.com>
2018-09-11 20:32:38 -0300

    Update testcalc.py

--- testcalc.py
+++ testcalc.py
@@ -6,7 +6,5 @@ def tesscalc():
 	assert soma(-1, 1) == 0
 	assert soma('1', 1) == 2
 	assert soma('-1', 1) == 0
-	assert soma(1, 'a') == false
-	assert soma('a', 'b') == false
 	assert soma(1.5, 1) == 2.5
 	assert soma('-1,5', 1) == -0.5
