a1499b1fe47f47d6c12c46bc89293d1b0d5eb608
rodolfohub <42880262+rodolfohub@users.noreply.github.com>
2018-09-11 20:44:27 -0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
 def soma(num1, num2):
-   num1 = input("Digite um numero: ")
-   num2 = input("Digite outro numero: ")
-   return float(num1) + float(num2)
+ num1 = input("Digite um numero: ")
+ num2 = input("Digite outro numero: ")
+ return float(num1) + float(num2)
