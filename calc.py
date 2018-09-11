d59417c7c16de60b05f838ec104e184a47f93597
rodolfohub <rodolfogmazevedo@gmail.com>
2018-09-11 13:28:15 -0300

    enviando repositorio

---
+++ calc.py
@@ -0,0 +1,4 @@
+def soma(num1, num2):
+   num1 = input("Digite um numero: ")
+   num2 = input("Digite outro numero: ")
+   print "A soma de %d + %d = %d "(num1, num2, num1 + num2)
---
+++ testcalc.py
@@ -0,0 +1,12 @@
+import pytest
+from calc import soma
+
+def tesscalc():
+	assert soma(1, 1) == 2
+	assert soma(-1, 1) == 0
+	assert soma('1', 1) == 2
+	assert soma('-1', 1) == 0
+	assert soma(1, 'a') == false
+	assert soma('a', 'b') == false
+	assert soma(1.5, 1) == 2.5
+	assert soma('-1,5', 1) == -0.5
