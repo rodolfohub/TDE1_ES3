3f15628244c28c074f63eb9ac70613988c7b3f9e
rodolfohub <rodolfogmazevedo@gmail.com>
2018-09-11 13:49:46 -0300

    atualizando codigo

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
 def soma(num1, num2):
    num1 = input("Digite um numero: ")
    num2 = input("Digite outro numero: ")
-   print "A soma de %d + %d = %d "(num1, num2, num1 + num2)
+   result=  num1 + num2
+   return result
